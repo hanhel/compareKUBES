63f1e80aa7fc9892d44abcb3387a3344695fe74d
Hanna Helgeland <hhelgela@gmail.com>
2022-06-27 11:55:22 +0200

    clear workspace

--- run_compare_kubes_newcols.R
+++ run_compare_kubes_newcols.R
@@ -1,5 +1,8 @@
 ## compareKUBES function
 
+# clear workspace
+rm(list = ls())
+
 # install pacman - a tool for easy installing and loading the other required packages
 if (!require("pacman")) install.packages("pacman")
 
@@ -11,12 +14,12 @@ pacman::p_load(dplyr, readr, tools, rlang)
 # file2 = older file
 
 # KOMMUNEHELSA
-#file1 <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/KOMMUNEHELSA/KH2022NESSTAR/BARNEVERN_TILTAK_2022-01-12-12-27.csv"
-#file2 <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/KOMMUNEHELSA/KH2021NESSTAR/BARNEVERN_TILTAK_2021-05-31-11-52.csv"
+file1 <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/KOMMUNEHELSA/KH2023NESSTAR/KMI_MFR_GK_2022-05-19-15-45.csv"
+file2 <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/KOMMUNEHELSA/KH2022NESSTAR/KMI_MFR_GK_2022-01-26-16-37.csv"
 
 # NORGESHELSA
-file1 <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/NORGESHELSA/NH2022NESSTAR/Totaldod_UTD_NH_LHF_5_2022-03-29-16-25.csv"
-file2 <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/NORGESHELSA/NH2021NESSTAR/Totaldod_UTD_NH_LHF_5_2021-08-31-09-45.csv"
+#file1 <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/NORGESHELSA/NH2022NESSTAR/Totaldod_NH_LHF_2022-03-29-16-24.csv"
+#file2 <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/NORGESHELSA/NH2021NESSTAR/Totaldod_NH_LHF_2021-08-30-16-15.csv"
 
 
 
